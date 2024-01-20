9a5925e7d559acf8c28d2cd60e49b9aba24b9b1f
JAYADEEPREDDYPEDDIREDDY <138111452+JAYADEEPREDDYPEDDIREDDY@users.noreply.github.com>
2024-01-20 10:53:40 +0530

    added animated cursor and made card animation in crew page
    added animated cursor and made card animation in crew page

--- src/pages/Crew Page/Crew.jsx
+++ src/pages/Crew Page/Crew.jsx
@@ -12,10 +12,22 @@ import sreenadh from "../../assets/sreenadh.jpg";
 import anil from '../../assets/anil.jpg';
 import Navbar from "../../components/Navbar/Navbar";
 import Bar from "../../components/Bar/Bar";
-    
+import { Tilt } from 'react-tilt'   
 
 const Crew = () => {   
+ 
 
+  const defaultOptions = {
+    reverse:        false,  // reverse the tilt direction
+    max:            35,     // max tilt rotation (degrees)
+    perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
+    scale:          1.1,    // 2 = 200%, 1.5 = 150%, etc..
+    speed:          1000,   // Speed of the enter/exit transition
+    transition:     true,   // Set a transition on enter/exit.
+    axis:           null,   // What axis should be disabled. Can be X or Y.
+    reset:          true,    // If the tilt effect has to be reset on exit.
+    easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+  }
     const teamMembers = [
         { name: 'B Dwishanth', role: 'Chief Excuitive Officer', imageSrc: dwisanth },
         { name: 'N Ganesh', role: 'Chief Finance Officer', imageSrc: saiganesh },
@@ -37,7 +49,9 @@ const Crew = () => {
     <div className='for-whole-Team-page'>
       <h1 className="for-heading-in-team">The Crew</h1>
       <div className="blockContainer" >
+      
         {teamMembers.map((member, index) => (
+          <Tilt options={defaultOptions}>
           <div className="blockInfo" key={index}>
             <div className="for-image">
               <div className="block-img">
@@ -47,7 +61,9 @@ const Crew = () => {
             <p>{member.name}</p>
             <h3>{member.role}</h3>
           </div>
+          </Tilt>
         ))}
+        
       </div>
     </div>
     </>
